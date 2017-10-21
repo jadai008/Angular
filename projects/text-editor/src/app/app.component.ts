6d27f0449de7a5aebaba173c36ee4ad77d2028b8
System Administrator <root@Ushas-MacBook-Air.local>
2017-10-21 23:58:10 +0530

    added title

--- projects/text-editor/src/app/app.component.ts
+++ projects/text-editor/src/app/app.component.ts
@@ -3,13 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  		<h1>{{title}}</h1>
   		<text-editor (countEmit)="updateWordCount($event)"></text-editor>
   		<h2>Word Count:</h2><h1>{{wordCount}}</h1>
   		`,
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app';
+  title = 'Simple Text Editor with word count display';
   wordCount: number = 0;
   updateWordCount(e:number): void {
   	this.wordCount = e;
