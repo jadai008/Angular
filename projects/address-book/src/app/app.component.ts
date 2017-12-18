39956f6c92a71fa705692638b79b4be974f307a7
Hariharan Lakshmi Narayanan <Hariharan.LakshmiNarayanan@halliburton.com>
2017-12-18 08:19:09 -0600

    changes

--- projects/address-book/src/app/app.component.ts
+++ projects/address-book/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from './model/contact.model';
 import { ContactsService } from './services/contacts.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy {
   
   title = 'app';
 
@@ -21,8 +23,17 @@ export class AppComponent implements OnInit{
 
   contacts: Contact[];
 
+  onAddSub: Subscription;
+
   ngOnInit(): void {
     this.contacts = this.contactsService.getContacts();
+    this.onAddSub = this.contactsService.contactAdded.subscribe((event: number) => {
+      this.contacts = this.contactsService.getContacts();
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.onAddSub.unsubscribe();
   }
 
   onContactSelected(event: Event, idx: number) {
